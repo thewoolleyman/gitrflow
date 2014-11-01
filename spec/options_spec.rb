d1959a9b63d94e7d26d34149e254a22cc5961d23
Chad Woolley <thewoolleyman@gmail.com>
2014-11-01 15:54:46 -0700

    add support for '--' option

--- spec/options_spec.rb
+++ spec/options_spec.rb
@@ -7,6 +7,12 @@ describe 'options' do
     ).to match(/Usage: /m)
   end
 
+  it 'ignores all options after --' do
+    expect(
+      process("#{gitrflow_path} -- --version", puts_output: false, expected_exit_status: 1)
+    ).to match(/^    --\t\tIgnore all following options/m)
+  end
+
   it '--version' do
     expect(process("#{gitrflow_path} --version", puts_output: false)).to match(/\d\.\d\.\d/)
     expect(process("#{gitrflow_path} -V", puts_output: false)).to match(/\d\.\d\.\d/)
