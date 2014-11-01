0a5a664961b67c18ba25babbc0875173f35a9d33
Chad Woolley <thewoolleyman@gmail.com>
2014-11-01 15:46:05 -0700

    better version option handling

--- spec/options_spec.rb
+++ spec/options_spec.rb
@@ -8,6 +8,10 @@ describe 'options' do
   end
 
   it '--version' do
-    expect(process("#{gitrflow_path} --version")).to match(/\d\.\d\.\d/)
+    expect(process("#{gitrflow_path} --version", puts_output: false)).to match(/\d\.\d\.\d/)
+    expect(process("#{gitrflow_path} -V", puts_output: false)).to match(/\d\.\d\.\d/)
+    expect(
+      process("#{gitrflow_path}", puts_output: false, expected_exit_status: 1)
+    ).to match(/^    -V, --version/m)
   end
 end
