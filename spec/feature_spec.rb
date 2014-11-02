383be69abdfd466871b3e9f65c8fd267a312c39b
Chad Woolley <thewoolleyman@gmail.com>
2014-11-02 16:01:49 -0700

    replace with calls to gitrflow_cmd

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -2,7 +2,7 @@ require_relative 'spec_helper'
 
 describe 'branch type parameter' do
   it 'fails if no command is specified' do
-    out = run("PATH=#{path_with_gitrflow} git rflow feature", out: false, exp_rc: 1)
+    out = run("#{gitrflow_cmd} feature", out: false, exp_rc: 1)
     expect(out).to match(/ERROR: The feature branch command is required./)
     expect(out).to match(/'git rflow --help' for usage./)
   end
@@ -10,7 +10,7 @@ end
 
 describe 'start' do
   it 'fails if no branch name is specified' do
-    out = run("PATH=#{path_with_gitrflow} git rflow feature start", out: false, exp_rc: 1)
+    out = run("#{gitrflow_cmd} feature start", out: false, exp_rc: 1)
     expect(out).to match(/ERROR: The feature branch name is required./)
     expect(out).to match(/'git rflow --help' for usage./)
   end
