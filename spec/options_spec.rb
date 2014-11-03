a3c62f00e1235e7c30301888de93dfa932e0cd25
Chad Woolley <thewoolleyman@gmail.com>
2014-11-02 22:54:41 -0700

    reorganize options spec

--- spec/options_spec.rb
+++ spec/options_spec.rb
@@ -1,35 +1,33 @@
 require_relative 'spec_helper'
 
 describe 'options' do
-  it 'prints error if no options are passed' do
-    expect(
-      run("#{gitrflow_cmd}", out: false, exp_rc: 1)
-    ).to match(/Usage: /m)
-  end
+  describe 'fails if' do
+    it 'unprocessed parameters exist' do
+      local_repo, _ = make_cloned_repo
+      FileUtils.cd(local_repo) do
+        cmd = "#{gitrflow_cmd} feature start feature1 unprocessed"
+        out = run(cmd, out: false, exp_rc: 1)
+        expect(out).to match(/ERROR: Unrecognized parameter 'unprocessed'/)
+      end
+    end
 
-  it 'ignores all options after --' do
-    expect(
-      run("#{gitrflow_cmd} -- --version", out: false, exp_rc: 1)
-    ).to match(/^    --\t\t\tIgnore all following options/m)
-  end
-
-  it 'processes options after commands' do
-    out = run("#{gitrflow_cmd} feature start feature1 --version", out: false)
-    expect(out).to match(version_regex)
-  end
-
-  it 'fails when unprocessed parameters exist' do
-    local_repo, _ = make_cloned_repo
-    FileUtils.cd(local_repo) do
-      cmd = "#{gitrflow_cmd} feature start feature1 unprocessed"
-      out = run(cmd, out: false, exp_rc: 1)
-      expect(out).to match(/ERROR: Unrecognized parameter 'unprocessed'/)
+    it 'invalid options exist' do
+      out = run("#{gitrflow_cmd} --invalid", out: false, exp_rc: 1)
+      expect(out).to match(/ERROR: Unrecognized parameter '--invalid'/)
     end
   end
 
-  it 'fails when invalid options exist' do
-    out = run("#{gitrflow_cmd} --invalid", out: false, exp_rc: 1)
-    expect(out).to match(/ERROR: Unrecognized parameter '--invalid'/)
+  describe 'behavior' do
+    it 'prints help if no options are passed' do
+      expect(
+        run("#{gitrflow_cmd}", out: false, exp_rc: 1)
+      ).to match(/Usage: /m)
+    end
+
+    it 'processes options after commands' do
+      out = run("#{gitrflow_cmd} feature start feature1 --version", out: false)
+      expect(out).to match(version_regex)
+    end
   end
 
   it '-h, --help' do
@@ -43,6 +41,12 @@ describe 'options' do
     expect(
       run("#{gitrflow_cmd}", out: false, exp_rc: 1)
     ).to match(/^    -V, --version/m)
+  end
+
+  it 'ignores all options after --' do
+    expect(
+      run("#{gitrflow_cmd} -- --version", out: false, exp_rc: 1)
+    ).to match(/^    --\t\t\tIgnore all following options/m)
   end
 
   def version_regex
