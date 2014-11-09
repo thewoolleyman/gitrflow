6560e6641588cadf9761c821ca2369d5f5d3327f
Chad Woolley <thewoolleyman@gmail.com>
2014-11-08 18:42:36 -0700

    split up feature specs into separate files

---
+++ spec/feature_start_spec.rb
@@ -0,0 +1,66 @@
+require_relative 'spec_helper'
+
+describe 'feature start' do
+  it 'is documented' do
+    help_text = 'feature start <branch_name>'
+    expect(run("#{gitrflow_path} -h", out: false, exp_rc: 1)).to match(/#{help_text}/)
+  end
+
+  describe 'fails if' do
+    it 'no branch name is specified' do
+      out = run("#{gitrflow_cmd} feature start", out: false, exp_rc: 1)
+      expect(out).to match(/ERROR: The feature branch name is required./)
+      expect(out).to match(/'git rflow --help' for usage./)
+    end
+
+    it 'local repo is not clean' do
+      local_repo, _ = make_cloned_repo
+      FileUtils.cd(local_repo) do
+        FileUtils.touch('dirty')
+        cmd = "#{gitrflow_cmd} feature start feature1"
+        out = run(cmd, out: false, exp_rc: 1)
+        expect(out).to match(/ERROR: Local repo is not clean. Please fix and retry./)
+      end
+    end
+
+    it 'local repo is "gone"' do
+      local_repo, _ = make_cloned_repo([])
+      FileUtils.cd(local_repo) do
+        FileUtils.touch('unpushed')
+        run('git add unpushed && git commit -m "unpushed"', out: false)
+        cmd = "#{gitrflow_cmd} feature start feature1"
+        out = run(cmd, out: false, exp_rc: 1)
+        expect(out).to match(/ERROR: Local repo is "gone". Please fix and retry./)
+      end
+    end
+
+    it 'local repo has unpushed changes' do
+      local_repo, _ = make_cloned_repo
+      FileUtils.cd(local_repo) do
+        FileUtils.touch('unpushed')
+        run('git add unpushed && git commit -m "unpushed"', out: false)
+        cmd = "#{gitrflow_cmd} feature start feature1"
+        out = run(cmd, out: false, exp_rc: 1)
+        expect(out).to match(/ERROR: Local repo has unpushed changes. Please fix and retry./)
+      end
+    end
+  end
+
+  it 'creates the specified feature branch' do
+    local_repo, _ = make_cloned_repo
+    branch = 'feature1'
+    expected_out = "Summary of actions:\n" \
+    "- A new branch '#{branch}' was created, based on 'master'\n" \
+    "- You are now on branch '#{branch}'\n\n" \
+    "Now, start committing on your feature. When done, use:\n\n" \
+    "     git flow feature finish #{branch}\n"
+
+    FileUtils.cd(local_repo) do
+      cmd = "#{gitrflow_cmd} feature start #{branch}"
+      out = run(cmd, out: false)
+      expect(out).to eq(expected_out)
+      git_status = run('git status', out: false)
+      expect(git_status).to match(/On branch #{branch}/)
+    end
+  end
+end
